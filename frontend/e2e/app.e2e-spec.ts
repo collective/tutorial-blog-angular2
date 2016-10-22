c23fbee63f3262a94fc533f000659a1906ce4c59
Timo Stollenwerk <tisto@plone.org>
2016-10-22 18:19:27 -0400

    Fix e2e test.

--- frontend/e2e/app.e2e-spec.ts
+++ frontend/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('frontend App', function() {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Plone Foundation');
   });
 });
