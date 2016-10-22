63e8b944a7abdf277d7b1836d81df487bf3a6565
Timo Stollenwerk <tisto@plone.org>
2016-10-21 22:09:01 -0400

    Basic auth implementation.

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserService } from './login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  constructor(private userService: UserService, private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  onSubmit(email, password) {
+    console.log('onSubmit');
+    /*this.userService.login(email, password).subscribe((result) => {
+      if (result) {
+        this.router.navigate(['']);
+      }
+    });*/
+    this.userService.login('admin', 'admin').subscribe(
+      data => {
+        if (data) {
+          this.router.navigate(['']);
+        }
+      },
+      err => console.log("Can't get page. Error code: %s, URL: %s ",
+                err.status, err.url),
+      () => console.log("Done")
+    );
   }
 
 }
---
+++ frontend/src/app/login/login.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+
+@Injectable()
+export class UserService {
+  private loggedIn = false;
+
+  constructor(private http: Http) {
+    this.loggedIn = !!localStorage.getItem('auth_token');
+  }
+
+  login(login: string, password: string): Observable<any> {
+    let headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.post(
+      'http://localhost:8080/Plone/@login',
+      JSON.stringify({
+        'login': login,
+        'password': password
+      }),
+      { headers }
+    )
+    .map(res => res.json())
+    .map((res) => {
+      if (res.success) {
+        localStorage.setItem('auth_token', res.auth_token);
+        this.loggedIn = true;
+      }
+
+      return res.success;
+    });
+  }
+
+  logout() {
+    localStorage.removeItem('auth_token');
+    this.loggedIn = false;
+  }
+
+  isLoggedIn() {
+    return this.loggedIn;
+  }
+}
