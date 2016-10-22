3ffe03ee5ee0105ca81120905911cb79a66de749
Eric BREHAULT <ebrehault@gmail.com>
2016-10-22 11:51:02 -0400

    fix test

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -2,6 +2,9 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {
+    RouterTestingModule
+} from '@angular/router/testing';
 
 describe('App: Frontend', () => {
   beforeEach(() => {
@@ -9,6 +12,7 @@ describe('App: Frontend', () => {
       declarations: [
         AppComponent
       ],
+      imports: [ RouterTestingModule ]
     });
   });
 
@@ -18,16 +22,10 @@ describe('App: Frontend', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have one header`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
+    const element = fixture.nativeElement;
     fixture.detectChanges();
-    let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(element.querySelectorAll('header').length).toBe(1);
   }));
 });
