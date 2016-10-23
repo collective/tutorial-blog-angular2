c34f7f9368cb315d0ce889325ad76b195b795dca
prakharjoshi <prakhar126@gmail.com>
2016-10-23 14:43:46 -0400

    Update home component

--- frontend/src/app/home/home.component.spec.ts
+++ frontend/src/app/home/home.component.spec.ts
@@ -2,10 +2,11 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
+import { HomepageService } from './home.service';
 
 describe('Component: Home', () => {
   it('should create an instance', () => {
-    let component = new HomeComponent();
+    let component = new HomeComponent(HomepageService);
     expect(component).toBeTruthy();
   });
 });
