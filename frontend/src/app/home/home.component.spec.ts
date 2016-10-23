807e7d7515b2c5a92aa82d1c8881dc10fdb55329
prakharjoshi <prakhar126@gmail.com>
2016-10-23 15:28:41 -0400

    Update code after linting

--- frontend/src/app/home/home.component.spec.ts
+++ frontend/src/app/home/home.component.spec.ts
@@ -6,7 +6,7 @@ import { HomepageService } from './home.service';
 
 describe('Component: Home', () => {
   it('should create an instance', () => {
-    let homeservice = new HomepageService
+    let homeservice = new HomepageService;
     let component = new HomeComponent(homeservice);
     expect(component).toBeTruthy();
   });
