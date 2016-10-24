148315b84619fa7cda08981aae639048e8e4df69
Timo Stollenwerk <tisto@plone.org>
2016-10-24 08:53:16 +0200

    Test pre-commit hook.

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -4,7 +4,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {
     RouterTestingModule
-} from '@angular/router/testing';
+} from "@angular/router/testing";
 
 describe('App: Frontend', () => {
   beforeEach(() => {
