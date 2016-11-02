d02fb5e6763c817e48ea3994fc46ad070f59a4e1
Victor Fernandez de Alba <sneridagh@gmail.com>
2016-11-02 09:15:54 +0100

    Fix linting

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from './login.service';
 
